3ed48e83bb32f60e4bf37ff7293608b8551077e5
bodanksy <adam.bodansky@gmail.com>
2017-03-25 17:43:15 +0100

    üzenet küldése backendről. Ha elindítjuk az alkalmazást és connectelünk és utána egy másik ablakon megnyitjuk a /websocket/other-page oldalt küld egy üzenetet amit /websocket/test oldal megjelenít

--- src/main/java/com/bodansky/controller/GreetingController.java
+++ src/main/java/com/bodansky/controller/GreetingController.java
@@ -8,8 +8,10 @@ import com.bodansky.domain.Greeting;
 import com.bodansky.domain.HelloMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GreetingController {
 
     private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public GreetingController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @GetMapping("/test")
     public String index() {
@@ -30,6 +39,14 @@ public class GreetingController {
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(3000); // simulated delay
-        return new Greeting("HelloMessage, " + message.getName() + "!");
+        return new Greeting("Hello " + message.getName() + "!");
+    }
+
+    @GetMapping("/other-page")
+    public String otherPage() {
+        log.info("otherPage() - send message from otherPage");
+        HelloMessage message = new HelloMessage("Adam");
+        messagingTemplate.convertAndSend("/topic/greetings",new Greeting("Hello " + message.getName() + "!"));
+        return "other-page";
     }
 }
