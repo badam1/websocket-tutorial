5aaef4dd3774a034e7b614e103726dd5650a137f
bodanksy <adam.bodansky@gmail.com>
2017-03-25 18:05:34 +0100

    lombokosítás ;)

--- src/main/java/com/bodansky/domain/Greeting.java
+++ src/main/java/com/bodansky/domain/Greeting.java
@@ -1,22 +1,16 @@
 package com.bodansky.domain;
 
+import lombok.*;
+
 /*
  * Created by Adam Bodansky on 2017.03.25..
  */
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Greeting {
 
     private String content;
-
-    public Greeting() {
-    }
-
-    public Greeting(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
 }
--- src/main/java/com/bodansky/domain/HelloMessage.java
+++ src/main/java/com/bodansky/domain/HelloMessage.java
@@ -4,24 +4,14 @@ package com.bodansky.domain;
  * Created by Adam Bodansky on 2017.03.25..
  */
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class HelloMessage {
 
-
     private String name;
-
-    public HelloMessage() {
-    }
-
-    public HelloMessage(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
