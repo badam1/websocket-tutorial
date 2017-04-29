5f7b120d1edba34d8f7016d54d11bc01e74e384f
bodanksy <adam.bodansky@gmail.com>
2017-04-29 19:33:26 +0200

    - refact video-chat.js

--- src/main/resources/static/js/video-chat.js
+++ src/main/resources/static/js/video-chat.js
@@ -9,8 +9,8 @@ var isConnected = false;
 $(function () {
 
     var $message = $("#message");
-    $message.text("Ready to connect!");
-    $message.fadeIn("slow");
+
+    showMessage();
 
     bindLocalCameraBtnOnClick();
     bindRemoteCameraBtnOnClick();
@@ -26,44 +26,207 @@ $(function () {
     var $body = $("body");
 
     $body.on("click", ".connectBtn", function () {
-        $("#local-mic").fadeIn("fast");
-        $("#local-vid").fadeIn("fast");
-        $("#remote-mic").fadeIn("fast");
-        $("#remote-vid").fadeIn("fast");
-        $(".header").fadeOut("fast");
-        $(".title").fadeIn("slow");
+        fadeInIconsFadeOutHeader();
         if (!isConnected) {
             $message.fadeOut("slow");
             isConnected = true;
             createConversation("testRoom");
-            $(this).removeClass("btn-info")
-                .removeClass("connectBtn")
-                .addClass("disconnectBtn")
-                .addClass("btn-warning")
-                .html("Reconnect");
+            changeConnectButton(this);
             stompClient.send("/app/connect", {}, JSON.stringify({'content': 'connected to testRoom'}));
         } else {
             $message.fadeOut("slow");
             joinConversation("testRoom");
-            $(this).removeClass("btn-info")
-                .removeClass("connectBtn")
-                .addClass("disconnectBtn")
-                .addClass("btn-warning")
-                .html("Reconnect");
+            changeConnectButton(this);
         }
     });
 
     $body.on("click", ".disconnectBtn", function () {
-        $("#local-mic").fadeOut("fast");
-        $("#local-vid").fadeOut("fast");
-        $("#remote-mic").fadeOut("fast");
-        $("#remote-vid").fadeOut("fast");
         leaveConversation();
         isConnected = false;
-        $(this).fadeOut("fast");
+        fadeOutIcons(this);
         stompClient.send("/app/leave", {}, JSON.stringify({'content': 'leave room'}));
     });
 });
+
+
+var roomCreated = function () {
+    isConnected = true;
+};
+
+var left = function () {
+    isConnected = false;
+    var $message = $("#message");
+    $message.text("Disconnected, reloading...")
+        .fadeIn("slow");
+    setTimeout(function () {
+        window.location.reload();
+    }, 1500);
+};
+
+var localVidFlag = false;
+var localMicFlag = false;
+var remoteMicFlag = false;
+var remoteVidFlag = false;
+
+var bindLocalCameraBtnOnClick = function() {
+    $('#local-vid').on('click', function () {
+        if (localVidFlag === true) {
+            enableLocalCamera();
+        } else {
+            disableLocalCamera();
+        }
+    });
+};
+
+var bindLocalMicrophoneOnClick = function() {
+    $('#local-mic').on('click', function () {
+        if (localMicFlag === true) {
+            enableLocalMicrophone();
+        } else {
+            disableLocalMicrophone();
+        }
+    });
+};
+
+var bindRemoteCameraBtnOnClick = function() {
+    $('#remote-vid').on('click', function () {
+        if (remoteVidFlag === true) {
+            enableRemoteCamera();
+        } else {
+            disableRemoteCamera();
+        }
+    });
+};
+
+var bindRemoteMicrophoneOnClick = function() {
+    $('#remote-mic').on('click', function () {
+        if (remoteMicFlag === true) {
+            enableRemoteMicrophone();
+        } else {
+            disableRemoteMicrophone();
+        }
+    });
+};
+
+var disableLocalCamera = function() {
+    localVidFlag = true;
+    $('#local-vid').css('color', 'red');
+    console.log("camera off");
+    $('.localStream').fadeOut("fast");
+    nextRTC.localStream.getVideoTracks()[0].enabled = false;
+};
+
+var enableLocalCamera = function() {
+    $('#local-vid').css('color', '#222222');
+    localVidFlag = false;
+    console.log("camera on");
+    $('.localStream').fadeIn("fast");
+    nextRTC.localStream.getVideoTracks()[0].enabled = true;
+};
+
+var enableLocalMicrophone = function() {
+    $('#local-mic').css('color', '#222222').removeClass("fa-microphone-slash").addClass("fa-microphone");
+    localMicFlag = false;
+    nextRTC.localStream.getAudioTracks()[0].enabled = true;
+};
+
+var disableLocalMicrophone = function() {
+    localMicFlag = true;
+    $('#local-mic').css('color', 'red').removeClass("fa-microphone").addClass("fa-microphone-slash");
+    nextRTC.localStream.getAudioTracks()[0].enabled = false;
+};
+
+var disableRemoteCamera = function() {
+    remoteVidFlag = true;
+    $('#remote-vid').css('color', 'red');
+    console.log("camera off");
+    $('.remoteStream').fadeOut("fast");
+};
+
+var enableRemoteCamera = function() {
+    $('#remote-vid').css('color', '#222222');
+    remoteVidFlag = false;
+    console.log("camera on");
+    $('.remoteStream').fadeIn("fast");
+};
+
+var enableRemoteMicrophone = function() {
+    $('#remote-mic').css('color', '#222222').removeClass("fa-volume-off").addClass("fa-volume-up");
+    remoteMicFlag = false;
+    $(".remote-vid").prop('muted', false);
+};
+
+var disableRemoteMicrophone = function() {
+    remoteMicFlag = true;
+    $('#remote-mic').css('color', 'red').removeClass("fa-volume-up").addClass("fa-volume-off");
+    $(".remote-vid").prop('muted', true);
+};
+
+var videoSizeFlagLoc = false;
+var videoSizeFlagRem = false;
+
+var bindChangeLocalStreamSize = function() {
+    $("body").on('click','.loc-vid-2', function () {
+        if (!videoSizeFlagLoc) {
+            videoSizeFlagLoc = true;
+            $(".local-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-9");
+            $(".remote-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-3");
+            $(".remote-vid").removeClass("rem-vid-2");
+        } else {
+            $(".remote-vid").addClass("rem-vid-2");
+            videoSizeFlagLoc = false;
+            $(".local-vid-wrapper").removeClass("col-xs-10").addClass("col-xs-offset-1").addClass("col-xs-5");
+            $(".remote-vid-wrapper").removeClass("col-xs-3").addClass("col-xs-offset-1").addClass("col-xs-5");
+        }
+    });
+};
+var bindChangeRemoteVideoSize = function() {
+    $("body").on('click','.rem-vid-2', function () {
+        if (!videoSizeFlagRem) {
+            videoSizeFlagRem = true;
+            $(".remote-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-9");
+            $(".local-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-3");
+            $(".local-vid").removeClass("loc-vid-2");
+        } else {
+            $(".local-vid").addClass("loc-vid-2");
+            videoSizeFlagRem = false;
+            $(".remote-vid-wrapper").removeClass("col-xs-10").addClass("col-xs-offset-1").addClass("col-xs-5");
+            $(".local-vid-wrapper").removeClass("col-xs-3").addClass("col-xs-offset-1").addClass("col-xs-5");
+        }
+    });
+};
+
+var fadeInIconsFadeOutHeader = function() {
+    $("#local-mic").fadeIn("fast");
+    $("#local-vid").fadeIn("fast");
+    $("#remote-mic").fadeIn("fast");
+    $("#remote-vid").fadeIn("fast");
+    $(".header").fadeOut("fast");
+    $(".title").fadeIn("slow");
+};
+
+var showMessage = function() {
+    var $message = $("#message");
+    $message.text("Ready to connect!");
+    $message.fadeIn("slow");
+};
+
+var fadeOutIcons = function(button) {
+    $("#local-mic").fadeOut("fast");
+    $("#local-vid").fadeOut("fast");
+    $("#remote-mic").fadeOut("fast");
+    $("#remote-vid").fadeOut("fast");
+    $(button).fadeOut("fast");
+};
+
+var changeConnectButton = function(button) {
+    $(button).removeClass("btn-info")
+        .removeClass("connectBtn")
+        .addClass("disconnectBtn")
+        .addClass("btn-warning")
+        .html("Reconnect");
+};
+
 
 var createConversation = function (roomId) {
     nextRTC.create(roomId);
@@ -139,158 +302,3 @@ nextRTC.on('left', function (nextRTC, event) {
 nextRTC.on('error', function (nextRTC, event) {
     console.log(JSON.stringify(event));
 });
-
-
-var roomCreated = function () {
-    isConnected = true;
-};
-
-var left = function () {
-    isConnected = false;
-    var $message = $("#message");
-    $message.text("Disconnected, reloading...")
-        .fadeIn("slow");
-        setTimeout(function () {
-        window.location.reload();
-    }, 1500);
-};
-
-var localVidFlag = false;
-var localMicFlag = false;
-
-
-var remoteMicFlag = false;
-var remoteVidFlag = false;
-
-function bindLocalCameraBtnOnClick() {
-    $('#local-vid').on('click', function () {
-
-        if (localVidFlag === true) {
-            enableLocalCamera();
-        } else {
-            disableLocalCamera();
-        }
-    });
-}
-
-function bindLocalMicrophoneOnClick() {
-    $('#local-mic').on('click', function () {
-
-        if (localMicFlag === true) {
-            enableLocalMicrophone();
-        } else {
-            disableLocalMicrophone();
-        }
-    });
-}
-
-function bindRemoteCameraBtnOnClick() {
-    $('#remote-vid').on('click', function () {
-
-        if (remoteVidFlag === true) {
-            enableRemoteCamera();
-        } else {
-            disableRemoteCamera();
-        }
-    });
-}
-
-function bindRemoteMicrophoneOnClick() {
-    $('#remote-mic').on('click', function () {
-
-        if (remoteMicFlag === true) {
-            enableRemoteMicrophone();
-        } else {
-            disableRemoteMicrophone();
-        }
-    });
-}
-
-function disableLocalCamera() {
-    localVidFlag = true;
-    $('#local-vid').css('color', 'red');
-    console.log("camera off");
-    $('.localStream').fadeOut("fast");
-    nextRTC.localStream.getVideoTracks()[0].enabled = false;
-}
-
-function enableLocalCamera() {
-    $('#local-vid').css('color', '#222222');
-    localVidFlag = false;
-    console.log("camera on");
-    $('.localStream').fadeIn("fast");
-    nextRTC.localStream.getVideoTracks()[0].enabled = true;
-}
-
-function enableLocalMicrophone() {
-    $('#local-mic').css('color', '#222222').removeClass("fa-microphone-slash").addClass("fa-microphone");
-    localMicFlag = false;
-    nextRTC.localStream.getAudioTracks()[0].enabled = true;
-}
-
-function disableLocalMicrophone() {
-    localMicFlag = true;
-    $('#local-mic').css('color', 'red').removeClass("fa-microphone").addClass("fa-microphone-slash");
-    nextRTC.localStream.getAudioTracks()[0].enabled = false;
-}
-
-function disableRemoteCamera() {
-    remoteVidFlag = true;
-    $('#remote-vid').css('color', 'red');
-    console.log("camera off");
-    $('.remoteStream').fadeOut("fast");
-}
-
-function enableRemoteCamera() {
-    $('#remote-vid').css('color', '#222222');
-    remoteVidFlag = false;
-    console.log("camera on");
-    $('.remoteStream').fadeIn("fast");
-}
-
-function enableRemoteMicrophone() {
-    $('#remote-mic').css('color', '#222222').removeClass("fa-volume-off").addClass("fa-volume-up");
-    remoteMicFlag = false;
-    $(".remote-vid").prop('muted', false);
-
-}
-
-function disableRemoteMicrophone() {
-    remoteMicFlag = true;
-    $('#remote-mic').css('color', 'red').removeClass("fa-volume-up").addClass("fa-volume-off");
-    $(".remote-vid").prop('muted', true);
-}
-
-var videoSizeFlagLoc = false;
-var videoSizeFlagRem = false;
-
-function bindChangeLocalStreamSize() {
-    $("body").on('click','.loc-vid-2', function () {
-        if (!videoSizeFlagLoc) {
-            videoSizeFlagLoc = true;
-            $(".local-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-9");
-            $(".remote-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-3");
-            $(".remote-vid").removeClass("rem-vid-2");
-        } else {
-            $(".remote-vid").addClass("rem-vid-2");
-            videoSizeFlagLoc = false;
-            $(".local-vid-wrapper").removeClass("col-xs-10").addClass("col-xs-offset-1").addClass("col-xs-5");
-            $(".remote-vid-wrapper").removeClass("col-xs-3").addClass("col-xs-offset-1").addClass("col-xs-5");
-        }
-    });
-}
-function bindChangeRemoteVideoSize() {
-    $("body").on('click','.rem-vid-2', function () {
-        if (!videoSizeFlagRem) {
-            videoSizeFlagRem = true;
-            $(".remote-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-9");
-            $(".local-vid-wrapper").removeClass("col-xs-5").removeClass("col-xs-offset-1").addClass("col-xs-3");
-            $(".local-vid").removeClass("loc-vid-2");
-        } else {
-            $(".local-vid").addClass("loc-vid-2");
-            videoSizeFlagRem = false;
-            $(".remote-vid-wrapper").removeClass("col-xs-10").addClass("col-xs-offset-1").addClass("col-xs-5");
-            $(".local-vid-wrapper").removeClass("col-xs-3").addClass("col-xs-offset-1").addClass("col-xs-5");
-        }
-    });
-}
